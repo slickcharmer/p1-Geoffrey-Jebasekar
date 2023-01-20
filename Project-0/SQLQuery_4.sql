d69c8f6828564a056b457db6a0cde2360e0cd432
slickcharmer <bkentee741@gmail.com>
2023-01-20 16:07:48 +0530

    Query commit

--- Project-0/SQLQuery_4.sql
+++ Project-0/SQLQuery_4.sql
@@ -1,44 +1,46 @@
 -- create schema project0;
-create table project0.TrainerSignup(
-userid int NOT NULL,
+create table Signup(
+-- userid int NOT NULL,
 firstname varchar(15) not null,
 lastname VARCHAR(15) not null,
 emailId VARCHAR(25) not NULL,
 [password] VARCHAR(15) not NULL,
-phoneno BIGINT not null,
-age TINYINT not NULL,
+phoneno VARCHAR(10) not null,
+age int not NULL,
 city VARCHAR(15) not null,
-constraint [PK_Signup] PRIMARY KEY(userid) 
+constraint [PK_Signup] PRIMARY KEY(emailId) 
 
 );
 -- drop table project0.TrainerSignup;
-create table project0.[Login]
+create table [Login]
 (
-    [UserLoginId] int not null,
-    emaild VARCHAR(25) not null,
+    --[UserLoginId] int not null,
+    emailid VARCHAR(25) not null,
     [password] VARCHAR(25) not null,
-    CONSTRAINT [FK_Login]FOREIGN KEY( UserLoginid ) REFERENCES project0.TrainerSignup(userid) on DELETE CASCADE on UPDATE CASCADE
+    CONSTRAINT [FK_Login]FOREIGN KEY( emailid ) REFERENCES Signup(emailId) on DELETE CASCADE on UPDATE CASCADE
 
 
 
 );
 
-CREATE TABLE project0.Education
+CREATE TABLE Education
 (
-    trainerid int not null,
+    --trainerid int not null,
+    emailid VARCHAR(25) not NULL,
     educationType VARCHAR(20) not null,
     instituteName VARCHAR(50) not null,
     stream VARCHAR(20) not NULL,
     startYear VARCHAR(4) not null,
     endYear VARCHAR(4) not null,
     [percentage] VARCHAR(4) not NULL,
-    CONSTRAINT [FK_Education] FOREIGN KEY(trainerid) REFERENCES project0.TrainerSignup(userid) on DELETE CASCADE on UPDATE CASCADE
+    CONSTRAINT [FK_Education] FOREIGN KEY(emailid) REFERENCES Signup(emailId) on DELETE CASCADE on UPDATE CASCADE
 
 );
 
-create table project0.Companies
+create table Companies
 (
-    trainerid int not NULL,
+    --trainerid int not NULL,
+    emailid VARCHAR(25) not NULL,
     companyName VARCHAR(25) not null,
     Title VARCHAR(10) not null,
     industry VARCHAR(20) not null,
@@ -46,17 +48,20 @@ create table project0.Companies
     [location] VARCHAR(20) not null,
     startYear VARCHAR(4) not null,
     endYear VARCHAR(4) not NULL
-    CONSTRAINT [FK_Companies] FOREIGN KEY(trainerid) REFERENCES project0.TrainerSignup(userid) on delete CASCADE on UPDATE CASCADE
+    CONSTRAINT [FK_Companies] FOREIGN KEY(emailid) REFERENCES Signup(emailid) on delete CASCADE on UPDATE CASCADE
 
 );
 
-CREATE table project0.Skills
+CREATE table Skills
 (
-    trainerid int not NULL,
+    emailid VARCHAR(25) not NULL,
     skill VARCHAR(15) not null,
-    Profeciency int not NULL CHECK(Profeciency>=1 & Profeciency<=5),
-    CONSTRAINT [FK_Skills] FOREIGN key(trainerid) REFERENCES project0.TrainerSignup(userid) on DELETE cascade on UPDATE CASCADE
+    Profeciency int not NULL ,
+    CONSTRAINT [FK_Skills] FOREIGN key(emailid) REFERENCES Signup(emailId) on DELETE cascade on UPDATE CASCADE
 
 
 
 );
+
+alter table Signup alter COLUMN age int not null
+alter TABLE Signup alter COLUMN phoneno VARCHAR(10) not null
